d0a5feb5aca08381e33c6131fa26c68526be765c
Muhammed Ashfar <muhammed.ashfar.7@gmail.com>
2024-11-10 18:19:12 +0530

    upload #3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
-document.getElementById('load-more').addEventListener('click', function() {
-    let currentCount = document.querySelectorAll('.post-list article').length;
-    fetch('/posts.json')
-      .then(response => response.json())
-      .then(data => {
-        let posts = data.slice(currentCount, currentCount + 10);
-        let postList = document.querySelector('.post-list');
-        posts.forEach(post => {
-          let article = document.createElement('article');
-          article.innerHTML = `
-            <h2><a href="${post.url}">${post.title}</a></h2>
-            <p>${post.excerpt}</p>
-            <img data-src="${post.image}" class="lazyload" alt="${post.title}">
-          `;
-          postList.appendChild(article);
-        });
-        if (currentCount + 10 >= data.length) {
-          document.getElementById('load-more').style.display = 'none';
-        }
-      });
-  });
-
   document.getElementById('search-bar').addEventListener('input', function() {
     const query = this.value;
     if (query.length > 2) {
